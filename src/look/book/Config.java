d4359926a4269e794f3a1549182d6f1039177db5
juve <juve-guz@gmail.com>
2012-11-27 17:26:19 -0600

    actualizando el gitignore

--- src/look/book/Config.java
+++ src/look/book/Config.java
@@ -5,6 +5,6 @@ package look.book;
 public class Config {
     public static String url="jdbc:mysql://localhost:3306/libros";
     public static String usuario="root";
-    public static String clave="";
+    public static String clave="root";
 
 }
