74e11efd38e3ac8c04f12b69b502bbe157c257d1
juve <juve-guz@gmail.com>
2012-11-27 19:11:00 -0600

    ush

--- src/look/book/Config.java
+++ src/look/book/Config.java
@@ -3,7 +3,7 @@ package look.book;
 
 
 public class Config {
-    public static String url="jdbc:mysql://localhost:3306/libros";
+    public static String url="jdbc:mysql://localhost:3306/Carrito";
     public static String usuario="root";
     public static String clave="root";
 
